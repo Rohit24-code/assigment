8649c92e9b39a9f6c0ede33b50e02d189c1c2b65
Rohit24-code <rohitsingh17031995@gmail.com>
2022-04-30 17:29:24 +0530

    assigment

---
+++ components/nav.js
@@ -0,0 +1,16 @@
+function nav(){
+    return`   
+     <h2>Macbook pro</h2>
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Portfolio</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Blog</li>
+            <li>Contact</li>
+        </ul>
+    </div> 
+    `
+}
+export {nav}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {nav} from "../components/nav"
+
+import '../styles/main.css'
+
+document.getElementById("nav").innerHTML=nav()
